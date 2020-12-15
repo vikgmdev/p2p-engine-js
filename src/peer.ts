c262235cd7b07b01c72cf4c1e3591120e12f9e6e
Victor Garcia <vikgm.dev@gmail.com>
2020-12-15 11:35:05 -0600

    Implement a peer base class

--- src/peer.ts
+++ src/peer.ts
@@ -2,13 +2,27 @@ import { EventEmitter } from 'events';
 import net from 'net';
 import * as lpmessage from './lpmessage';
 
+interface PeerNode {
+  id: string;
+  host?: string;
+  port?: number;
+  retries?: number;
+  reconnectTimeout?: NodeJS.Timeout;
+  pendingSocket?: net.Socket;
+  socket?: net.Socket;
+}
+
+interface Peers {
+  [peer: string]: PeerNode;
+}
+
 export default class Peer extends EventEmitter {
   me: string | Buffer;
-  peers: {};
+  peers: Peers = {};
 
-  server: net.Server;
+  server?: net.Server;
 
-  protected onconnection(socket: net.Socket) {
+  onconnection = (socket: net.Socket) => {
     this.errorHandle(socket);
 
     lpmessage.read(socket, (from) => {
@@ -21,9 +35,9 @@ export default class Peer extends EventEmitter {
       this.attachCleanup(peer, socket);
       this.onready(peer, socket);
     });
-  }
+  };
 
-  protected connect(peer, socket?: net.Socket) {
+  connect = (peer: PeerNode, socket?: net.Socket) => {
     if (peer.socket || peer.pendingSocket) return socket && socket.destroy();
     if (peer.reconnectTimeout) clearTimeout(peer.reconnectTimeout);
 
@@ -36,22 +50,20 @@ export default class Peer extends EventEmitter {
     this.errorHandle(socket);
     this.attachCleanup(peer, socket);
 
-    lpmessage.read(socket, function () {
-      this.onready(peer, socket);
-    });
-  }
+    lpmessage.read(socket, () => this.onready(peer, socket));
+  };
 
-  private onready(peer, socket: net.Socket) {
+  private onready = (peer, socket: net.Socket) => {
     socket.setTimeout(0); // reset timeout
-    var oldSocket = peer.socket;
+    const oldSocket = peer.socket;
     peer.retries = 0;
     peer.socket = socket;
     peer.pendingSocket = null;
     if (oldSocket) oldSocket.destroy();
     this.emit('connection', peer.socket, peer.id);
-  }
+  };
 
-  private attachCleanup(peer, socket: net.Socket) {
+  private attachCleanup = (peer, socket: net.Socket) => {
     socket.on('close', () => {
       if (peer.socket === socket) peer.socket = null;
       if (peer.pendingSocket === socket) peer.pendingSocket = null;
@@ -65,11 +77,11 @@ export default class Peer extends EventEmitter {
       peer.reconnectTimeout = setTimeout(reconnect, (1 << peer.retries) * 250);
       this.emit('reconnect', peer.id, peer.retries);
     });
-  }
+  };
 
-  private errorHandle(socket: net.Socket) {
+  private errorHandle = (socket: net.Socket) => {
     socket.on('error', () => socket.destroy());
 
     socket.setTimeout(15000, () => socket.destroy()); // 15s to do the handshake
-  }
+  };
 }
