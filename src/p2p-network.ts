7783467ac290358ac6c082b3f530eb41d970857d
Victor Garcia <vikgm.dev@gmail.com>
2020-12-15 11:35:29 -0600

    Implement a new p2p network class

--- src/p2p-network.ts
+++ src/p2p-network.ts
@@ -1,13 +1,11 @@
 import net from 'net';
 import Peer from './peer';
 
-export default class P2P extends Peer {
+export default class P2PNetwork extends Peer {
   constructor(me: string = '', peers: string[] = []) {
     super();
 
     this.me = me;
-    this.peers = peers;
-    this.server = null;
 
     const [, port] = me.split(':');
 
@@ -15,26 +13,22 @@ export default class P2P extends Peer {
     if (this.me) this.listen(Number(port));
 
     // Connect to peers
-    if (peers) peers.forEach(this.add.bind(this));
+    if (peers) peers.forEach(this.add);
   }
 
-  get connections() {
-    var peers = this.peers;
+  listen = (port: number) => {
+    this.server = net.createServer(this.onconnection);
+    this.server.listen(port);
+  };
+
+  public get connections(): net.Socket[] {
+    const peers = this.peers;
     return Object.keys(peers)
       .map((id) => peers[id].socket)
       .filter((socket) => socket);
   }
 
-  peer(addr: string) {
-    return (this.peers[addr] && this.peers[addr].socket) || null;
-  }
-
-  listen(port: number) {
-    this.server = net.createServer(this.onconnection);
-    this.server.listen(port);
-  }
-
-  add(addr: string) {
+  add = (addr: string) => {
     if (addr === this.me) return;
 
     const [host, port] = addr.split(':');
@@ -47,13 +41,13 @@ export default class P2P extends Peer {
     peer.pendingSocket = peer.pendingSocket || null;
     peer.socket = peer.socket || null;
 
-    connect(this, peer);
-  }
+    this.connect(peer);
+  };
 
-  remove(addr: string) {
+  remove = (addr: string) => {
     if (addr === this.me) return;
 
-    var peer = this.peers[addr];
+    const peer = this.peers[addr];
     if (!peer) return;
 
     delete this.peers[addr];
@@ -61,10 +55,10 @@ export default class P2P extends Peer {
     if (peer.socket) peer.socket.destroy();
     if (peer.pendingSocket) peer.pendingSocket.destroy();
     clearTimeout(peer.reconnectTimeout);
-  }
+  };
 
-  destroy() {
+  destroy = () => {
     if (this.server) this.server.close();
-    Object.keys(this.peers).forEach(this.remove.bind(this));
-  }
+    Object.keys(this.peers).forEach(this.remove);
+  };
 }
